b8b0485e447811c7da8c1abbc029b8801ad271c3
Vanessa <vanessa.nunez.p@gmail.com>
2022-12-20 22:17:09 -0300

    :zap: improve script to create table to docker

--- scripts/initdb.sql
+++ scripts/initdb.sql
@@ -1,38 +1,54 @@
 
-create table user(
+create table login(
     id serial primary key,
     email varchar(100) unique NOT NULL,
     password varchar(30) NOT NULL,
-)
+    last_login TIMESTAMP 
+);
 
-create table profile(
+create table account(
     id serial primary key,
     user_id int NOT NULL,
     name varchar(200) NOT NULL,
     user_name varchar(100) NOT NULL,
     birth_date date,
-    foreign key (user_id) REFERENCES user (id)
-    create_at date,
-    update_at date,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    foreign key (user_id) REFERENCES login (id)
+);
+
+create table roles(
+    id serial primary key,
+    role_name varchar(50)
 )
+
+create table account_role(
+    user_id int not null,
+    role_id int not null,
+    grant_date TIMESTAMP NOT NULL,
+    primary key (user_id, role_id),
+    foreign key (role_id) REFERENCES roles (id),
+    foreign key (user_id) REFERENCES account (id)
+)
+
 
 create table follower(
     id serial primary key,
     user_id int NOT NULL,
     follow_id int NOT NULL,
-    foreign key (user_id) REFERENCES user (id),
-    foreign key (follow_id) REFERENCES user (id),
-    create_at date,
-    update_at date,
+    foreign key (user_id) REFERENCES login (id),
+    foreign key (follow_id) REFERENCES login (id),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 )
 
 create table photo_profile(
     id serial primary key,
     user_id int NOT NULL,
     description varchar(1000) NOT NULL,
-    foreign key (user_id) REFERENCES user (id),
-    create_at date,
-    update_at date,
+    foreign key (user_id) REFERENCES login (id),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 )
 
 create table publication(
@@ -41,8 +57,8 @@ create table publication(
     description varchar(1000),
     image varchar(100),
     foreign key (profile_id) REFERENCES profile (id),
-    create_at date,
-    update_at date,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 )
 
 create table comment(
@@ -50,16 +66,16 @@ create table comment(
     publication_id int NOT NULL,
     description varchar(1000) NOT NULL,
     foreign key (publication_id) REFERENCES publication (id),
-    create_at date,
-    update_at date,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 )
 
 create table reactions(
     id serial primary key,
     emoticon varchar(100),
     name varchar(50),
-    create_at date,
-    update_at date,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 )
 
 create table reaction_post(
@@ -68,6 +84,6 @@ create table reaction_post(
     reaction_id int NOT NULL,
     foreign key (publication_id) REFERENCES publication (id),
     foreign key (reaction_id) REFERENCES reactions (id),
-    create_at date,
-    update_at date,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 )
